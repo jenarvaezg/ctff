0d1905aea3b533d3d48e8948246e898e03001cf7
root <root@localhost.localdomain>
2015-11-15 02:25:55 +0100

    Added /user/:username, and more stuff

--- test_server.go
+++ test_server.go
@@ -7,66 +7,93 @@ import (
     "strconv"
     "net/http"
     "html/template"
-//     "strings"
     "regexp"
+    "strings"
     "log"
+    "errors"
     "crypto/sha512"
-    "crypto/md5"
     "encoding/base64"
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
 )
 
+type user struct{
+	Email string
+	Username string
+	Created string
+	IsYou bool
+}
+
+
 
 func handlerRoot(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("html/root.html")
     t.Execute(w, nil)
-    //fmt.Fprintf(w, "Hello andrei!") // send data to client side
 }
 
-func addUser(user, password string) {
+func addUser(mail, password, username string) error {
 	db, err := sql.Open("mysql", "root@/tfg?charset=utf8")
     checkErr(err)
     defer db.Close()
 
      //insert
-    stmt, err := db.Prepare("INSERT userinfo SET email=?,password=?,created=?")
+    stmt, err := db.Prepare("INSERT userinfo SET email=?,password=?,created=?,username=?")
     checkErr(err)
 
     date := time.Now().String()
 
-    res, err := stmt.Exec(user, password, date)
-    checkErr(err)
-
-    id, err := res.LastInsertId()
-    checkErr(err)
-
-    fmt.Println(id)
+    _, err = stmt.Exec(mail, password, date, username)
+    return err
 
 }
 
-func getUser(email, password string) {
+func verifyUser(identifier, password string) (u user, err error) {
 	db, err := sql.Open("mysql", "root@/tfg?charset=utf8")
 	checkErr(err)
 	defer db.Close()
 
-	stmt, err := db.Prepare("SELECT * FROM userinfo WHERE email=? and password=?")
+	stmt, err := db.Prepare(
+		"SELECT * FROM userinfo WHERE (email=? or username=?) and password=?")
 	checkErr(err)
 
-	fmt.Printf("Trying with %s:%s\n", email, password)
-	rows, err := stmt.Query(email, password)
+	rows, err := stmt.Query(identifier, identifier, password)
 	checkErr(err)
 
-	for rows.Next() {
-        var email string
-        var password string
-        var created string
-        err = rows.Scan(&email, &password, &created)
-        checkErr(err)
-        fmt.Println(email)
-        fmt.Println(password)
-        fmt.Println(created)
+	if !rows.Next() {
+		return
+	}
+
+	var disposable string
+
+	err = rows.Scan(&u.Email, &disposable, &u.Created, &u.Username)
+
+    return
+
+}
+
+func findUser(username string) (u user, err error){
+
+	db, err := sql.Open("mysql", "root@/tfg?charset=utf8")
+	checkErr(err)
+	defer db.Close()
+
+	stmt, err := db.Prepare(
+		"SELECT * FROM userinfo WHERE username=?")
+	checkErr(err)
+
+	rows, err := stmt.Query(username)
+	
+
+    if !rows.Next(){
+    	err = errors.New("Not found")
+    	return
     }
+
+    var disposable string
+
+    err = rows.Scan(&u.Email, &disposable, &u.Created, &u.Username)
+
+    return 
 
 }
 
@@ -77,15 +104,19 @@ func checkErr(err error) {
 }
 
 func validEmail(email string) bool {
-	match , _ := regexp.MatchString(`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`, email)
+	match , _ := regexp.MatchString(
+		`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`, email)
 	return match
 }
 
-func validCSRF(token string) bool {
-	if token == "" {
+func validCSRF(r *http.Request) bool {
+	token := r.Form.Get("csrf")
+	cookie, err := r.Cookie("csrf")
+	if token == ""  || err != nil || token != cookie.Value {
         	return false
 	}
-	//do later
+
+	
 	return true
 }
 
@@ -100,17 +131,19 @@ func validPassword(password string) bool {
 
 func giveFormTemplate(path string, w http.ResponseWriter) {
 	crutime := time.Now().Unix()
-    h := md5.New()
+    h := sha512.New()
     io.WriteString(h, strconv.FormatInt(crutime, 10))
     token := fmt.Sprintf("%x", h.Sum(nil))
     t, _ := template.ParseFiles(path)
+    expiration := time.Now().Add(1 * time.Hour)
+	cookie := http.Cookie{Name: "csrf", Value: token, Expires: expiration}
+	http.SetCookie(w, &cookie)
     t.Execute(w, token)
 }
 
 func getSha512(s string) string {
 	hasher := sha512.New()
     hasher.Write([]byte(s))
-    fmt.Printf("Going to sha: %s\n", s)
     return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 }
 
@@ -120,7 +153,7 @@ func handlerCreateAccount(w http.ResponseWriter, r *http.Request) {
     } else if r.Method == "POST" {
         r.ParseForm()
         
-        if token := r.Form.Get("csrf"); !validCSRF(token){
+        if !validCSRF(r){
         	fmt.Fprintf(w, "ANTI CSRF GOT SOMETHING")
         	return
         }        
@@ -129,19 +162,23 @@ func handlerCreateAccount(w http.ResponseWriter, r *http.Request) {
         	fmt.Fprintf(w, "Please input a valid email")
         	return
         }
+        username := string(template.HTMLEscapeString(r.Form.Get("username")))
+        if username == "" {
+        	fmt.Fprintf(w, "Username can't be blank")
+        	return
+        }
         password := r.Form.Get("password")
         if !validPassword(password) {
         	fmt.Fprintln(w, "Given password does not comply with ",
         				"given password directives")
         	return
         }
-
 		password = getSha512(r.Form.Get("password"))        
-
-        addUser(email, password)
-
-        
-        fmt.Fprintln(w, "OK")
+        if err := addUser(email, password, username); err != nil {
+        	fmt.Fprintf(w, "Mail or username already in use or %s\n", err)
+        	return
+        }
+        http.Redirect(w, r, "/created", 301)
     }
 }
 
@@ -150,21 +187,65 @@ func handlerLogin(w http.ResponseWriter, r *http.Request) {
 		giveFormTemplate("html/login.html", w)
 	} else if r.Method == "POST" {
 		r.ParseForm()
-		if token := r.Form.Get("csrf"); !validCSRF(token){
+		if !validCSRF(r){
 			fmt.Fprintf(w, "ANTI CSRF GOT SOMETHING")
 			return
 		}
-		email := r.Form.Get("email")
+		identifier := r.Form.Get("identifier")
 		password := getSha512(r.Form.Get("password"))
 
-		getUser(email, password)
+		user, err := verifyUser(identifier, password)
+
+		if err != nil {
+			fmt.Fprintf(w, "Wrong mail or password, %s\n", err)
+			return
+		}
+		expiration := time.Now().Add(7 * 24 * time.Hour)
+		cookie := http.Cookie{Name: "session", Value: user.Username, Expires: expiration,
+							HttpOnly: true}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/user/" + user.Username, 301)
+
 	}
+}
+
+func handlerUser(w http.ResponseWriter, r *http.Request) {
+	username := strings.Split(r.URL.Path[1:], "/")[1]
+	if username == "" {
+		fmt.Fprintf(w, "User directory goes here\n")
+		return
+	}
+	user, err := findUser(username)
+	if err != nil {
+		fmt.Fprintf(w, "User %s not found :(\n", username)
+		return
+	}
+	
+
+	cookie, err := r.Cookie("session")
+	if err != nil {
+		fmt.Fprintf(w, "You must be logged in!\n")
+		return
+	}
+	if cookie.Value == user.Username {
+		user.IsYou = true
+	}
+	t, _ := template.ParseFiles("html/user.html")
+    t.Execute(w, user)	
+
+}
+
+func handlerCreated(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("html/created.html")
+    t.Execute(w, nil)
 }
 
 func main() {
     http.HandleFunc("/", handlerRoot) // set router
     http.HandleFunc("/create_account", handlerCreateAccount)
     http.HandleFunc("/login", handlerLogin)
+    http.HandleFunc("/user/", handlerUser)
+    http.HandleFunc("/created", handlerCreated)
     err := http.ListenAndServe(":9090", nil) // set listen port
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
