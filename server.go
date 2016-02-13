6e2d6b8eed71d61078f3535f7f7dee9b580c2e3d
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2016-02-13 12:07:28 -0500

    First time with bootstrap!

--- server.go
+++ server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha512"
 	"encoding/base64"
 	"errors"
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -14,10 +16,16 @@ import (
 	"os/exec"
 	"regexp"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+)
+
+const (
+	setup = iota
+	run   = iota
 )
 
 type user struct {
@@ -30,17 +38,17 @@ type user struct {
 }
 
 type challenge struct {
-	Title        string
-	Description  template.HTML
-	Category     string
-	Id           int
-	MaxScore     int
+	Title       string
+	Description template.HTML
+	Category    string
+	Id          int
+	MaxScore    int
 	/*Solution     string
 	SolutionType string*/
-	Hints        []string
-	Path         string
-	Creator      string
-	LaunchText   template.HTML
+	Hints      []string
+	Alias      string
+	Creator    string
+	LaunchText template.HTML
 }
 
 type challenge_link struct {
@@ -53,34 +61,31 @@ type challenge_link struct {
 }
 
 func (c challenge) launch(user string) template.HTML {
-	out, err := exec.Command(c.Path+"/rc/start_challenge", user).Output()
+	out, err := exec.Command(ChallengesPath+"/"+c.Alias+
+		"/rc/start_challenge", user).Output()
 	if err != nil {
-		return template.HTML(err.Error())
+		return template.HTML(err.Error() + string(out))
 	}
 	return template.HTML(out)
 }
 
 func (c challenge) stop(user string) {
-	_, err := exec.Command(c.Path+"/rc/stop_challenge", user).Output()
+	_, err := exec.Command(ChallengesPath+"/"+c.Alias+
+		"/rc/stop_challenge", user).Output()
 	if err != nil {
 		log.Println(template.HTML(err.Error()))
 	}
 }
 
-
 func (c challenge) checkSolution(solution, user string) bool {
-	/*if c.SolutionType == "match" {
-		return c.Solution == solution
-	}
-	if c.SolutionType == "regex" {
-		return true //TODO
-	}*/
-	err := exec.Command(c.Path+"/rc/check_solution", solution, user).Run()
+	out, err := exec.Command(ChallengesPath+"/"+c.Alias+
+		"/rc/check_solution", solution, user).Output()
+	fmt.Println(out, err)
 	return err == nil
 }
 
 func (c challenge) addToEnvironment() error {
-	path := ChallengesPath + "/" + c.Path
+	path := ChallengesPath + "/" + c.Alias
 	err := os.MkdirAll(path, os.FileMode(0755))
 	if err != nil {
 		fmt.Println(err)
@@ -92,7 +97,6 @@ func (c challenge) addToEnvironment() error {
 	f, _ = os.Create(path + "/rc/stop_challenge")
 	f.Chmod(0777)
 	f.Close()
-	//if c.SolutionType == "external" {
 	f, _ = os.Create(path + "/rc/check_solution")
 	f.Chmod(0777)
 	f.Close()
@@ -102,6 +106,7 @@ func (c challenge) addToEnvironment() error {
 func challengeFromForm(form url.Values) (challenge, error) {
 	var err error
 	c := challenge{}
+	fmt.Println(form)
 	c.Title = form.Get("title")
 	if c.Title == "" {
 		return c, errors.New("No Title")
@@ -110,8 +115,8 @@ func challengeFromForm(form url.Values) (challenge, error) {
 	if c.Title == "" {
 		return c, errors.New("No Description")
 	}
-	c.Path = form.Get("path")
-	if c.Path == "" {
+	c.Alias = form.Get("path")
+	if c.Alias == "" {
 		return c, errors.New("No path")
 	}
 	c.MaxScore, err = strconv.Atoi(form.Get("points"))
@@ -122,17 +127,6 @@ func challengeFromForm(form url.Values) (challenge, error) {
 	if c.Category == "" {
 		return c, errors.New("No Category")
 	}
-	/*c.SolutionType = form.Get("solution_type")
-	if c.SolutionType == "" {
-		return c, errors.New("No Solution Type")
-	}
-	if c.SolutionType == "external" {
-		return c, nil
-	}
-	c.Solution = form.Get("solution")
-	if c.Solution == "" {
-		return c, errors.New("No Solution")
-	}*/
 	return c, nil
 }
 
@@ -201,20 +195,20 @@ func handlerRoot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	session, _ := store.Get(r, "session")
-	t, _ := template.ParseFiles("html/root.html")
+	t, _ := template.ParseFiles("static/root.html")
 	t.Execute(w, session.Values["username"] != nil)
 }
 
 func handlerCreateAccount(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		giveFormTemplate("html/create_account.html", w)
+		giveFormTemplate("static/create_account.html", w)
 	} else if r.Method == "POST" {
 		r.ParseForm()
 
-		if !validCSRF(r) {
+		/*if !validCSRF(r) {
 			fmt.Fprintf(w, "ANTI CSRF GOT SOMETHING")
 			return
-		}
+		}*/
 		email := string(template.HTMLEscapeString(r.Form.Get("email")))
 		if !validEmail(email) {
 			fmt.Fprintf(w, "Please input a valid email")
@@ -243,13 +237,14 @@ func handlerCreateAccount(w http.ResponseWriter, r *http.Request) {
 func handlerLogin(w http.ResponseWriter, r *http.Request) {
 	session := checkNotLogged(w, r)
 	if r.Method == "GET" {
-		giveFormTemplate("html/login.html", w)
+		giveFormTemplate("static/login.html", w)
 	} else if r.Method == "POST" {
+
 		r.ParseForm()
-		if !validCSRF(r) {
+		/*if !validCSRF(r) {
 			fmt.Fprintf(w, "ANTI CSRF GOT SOMETHING")
 			return
-		}
+		}*/
 		identifier := r.Form.Get("identifier")
 		password := getSha512(r.Form.Get("password"))
 		username := verifyUser(identifier, password)
@@ -266,7 +261,7 @@ func handlerLogin(w http.ResponseWriter, r *http.Request) {
 
 func handlerUsers(w http.ResponseWriter, r *http.Request) {
 	users := getUsernames()
-	t, _ := template.ParseFiles("html/users.html")
+	t, _ := template.ParseFiles("static/users.html")
 	t.Execute(w, users)
 }
 
@@ -282,12 +277,12 @@ func handlerUser(w http.ResponseWriter, r *http.Request) {
 	if username == user.Username {
 		user.IsYou = true
 	}
-	t, _ := template.ParseFiles("html/user.html")
+	t, _ := template.ParseFiles("static/user.html")
 	t.Execute(w, user)
 }
 
 func handlerCreated(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("html/created.html")
+	t, _ := template.ParseFiles("static/created.html")
 	t.Execute(w, nil)
 }
 
@@ -301,7 +296,7 @@ func handlerLogout(w http.ResponseWriter, r *http.Request) {
 func handlerChallenges(w http.ResponseWriter, r *http.Request) {
 	checkLogged(w, r)
 	challenges := getChallengesLinks()
-	t, _ := template.ParseFiles("html/challenges.html")
+	t, _ := template.ParseFiles("static/challenges.html")
 	t.Execute(w, challenges)
 }
 
@@ -316,13 +311,12 @@ func handlerChallenge(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, err)
 		return
 	}
-	challenge.Path = ChallengesPath + "/" + challenge.Path
 	if userFinishedChallenge(user.Email, target) {
 		fmt.Fprintf(w, "You already finished this challenge!")
 		return
 	}
 	if r.Method == "GET" {
-		t, err := template.ParseFiles("html/challenge.html")
+		t, err := template.ParseFiles("static/challenge.html")
 		if err != nil {
 			fmt.Fprint(w, err)
 			return
@@ -332,7 +326,7 @@ func handlerChallenge(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		if r.Form.Get("launch") != "" {
 			challenge.LaunchText = challenge.launch(user.Username)
-			t, err := template.ParseFiles("html/challenge.html")
+			t, err := template.ParseFiles("static/challenge.html")
 			if err != nil {
 				fmt.Fprint(w, err)
 				return
@@ -341,7 +335,7 @@ func handlerChallenge(w http.ResponseWriter, r *http.Request) {
 		} else if r.Form.Get("stop") != "" {
 			challenge.stop(user.Username)
 			challenge.LaunchText = ""
-			t, err := template.ParseFiles("html/challenge.html")
+			t, err := template.ParseFiles("static/challenge.html")
 			if err != nil {
 				fmt.Fprint(w, err)
 				return
@@ -371,31 +365,32 @@ func handlerSuccess(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("NOT OK")
 		http.Redirect(w, r, "/", 301)
 	}
+	session.Options = &sessions.Options{MaxAge: -1, Path: "/"}
+	session.Save(r, w)
 	session = checkLogged(w, r)
+	fmt.Println(session)
 	username := session.Values["username"].(string)
-
+	fmt.Println("OK")
 	challenge, err := getChallenge(id)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer challenge.stop(username)
-	session.Options = &sessions.Options{MaxAge: -1, Path: "/"}
-	session.Save(r, w)
-	t, _ := template.ParseFiles("html/success.html")
+	t, _ := template.ParseFiles("static/success.html")
 	t.Execute(w, challenge)
 }
 
 func handlerRanking(w http.ResponseWriter, r *http.Request) {
 	checkLogged(w, r)
 	ranking := getRanking()
-	t, _ := template.ParseFiles("html/ranking.html")
+	t, _ := template.ParseFiles("static/ranking.html")
 	t.Execute(w, ranking)
 }
 
 func handlerAddChallenge(w http.ResponseWriter, r *http.Request) {
 	session := checkLogged(w, r)
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("html/add_challenge.html")
+		t, _ := template.ParseFiles("static/add_challenge.html")
 		t.Execute(w, nil)
 	} else if r.Method == "POST" {
 		r.ParseForm()
@@ -407,7 +402,7 @@ func handlerAddChallenge(w http.ResponseWriter, r *http.Request) {
 		challenge.Creator = session.Values["username"].(string)
 		challenge.addToEnvironment()
 		addChallenge(challenge)
-		t, err := template.ParseFiles("html/challenge_added.html")
+		t, err := template.ParseFiles("static/challenge_added.html")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -416,15 +411,24 @@ func handlerAddChallenge(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var store = sessions.NewCookieStore([]byte("EEEEH"))
-
-func main() {
-	var err error
+func handlerStatic(w http.ResponseWriter, r *http.Request) {
+	stream, err := ioutil.ReadFile(r.URL.Path[1:])
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		fmt.Fprintf(w, "File %s does not exist\n", r.URL.Path[1:])
 	}
-	r := mux.NewRouter()
+
+	b := bytes.NewBuffer(stream)
+
+	if _, err := b.WriteTo(w); err != nil { // <----- here!
+		fmt.Fprintf(w, "%s", err)
+	}
+}
+
+var store = sessions.NewCookieStore([]byte("EEEEH"))
+
+func setupRouter(r *mux.Router) {
 	r.HandleFunc("/", handlerRoot)
 	r.HandleFunc("/create_account", handlerCreateAccount)
 	r.HandleFunc("/login", handlerLogin)
@@ -436,9 +440,81 @@ func main() {
 	r.HandleFunc("/logout", handlerLogout)
 	r.HandleFunc("/success", handlerSuccess)
 	r.HandleFunc("/ranking", handlerRanking)
-	r.HandleFunc("/add_challenge", handlerAddChallenge)     
-	err = http.ListenAndServeTLS(":9090", "server.pem", "server.key", r)
+	r.HandleFunc("/add_challenge", handlerAddChallenge)
+	r.HandleFunc("/static/{folder}/{element}", handlerStatic)
+
+}
+
+func addNewChallenges() {
+	fileInfos, err := ioutil.ReadDir(ChallengesPath)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal(err)
 	}
+	aliases := getAllChallengeAliases()
+	old_challenges := make(map[string]bool)
+	for _, fileInfo := range fileInfos {
+		old_challenges[fileInfo.Name()] = false
+		for _, alias := range aliases {
+			if fileInfo.Name() == alias {
+				old_challenges[alias] = true
+				break
+			}
+		}
+	}
+	var wg sync.WaitGroup
+	for k, v := range old_challenges {
+		if !v {
+			wg.Add(1)
+			go func(dirname string) {
+				f, err := os.Open(dirname + "/info.json")
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				defer f.Close()
+				fmt.Println("OK")
+				wg.Done()
+			}(ChallengesPath + "/" + k)
+		}
+	}
+	wg.Wait()
+}
+
+func main() {
+	var err error
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if mode := checkArgs(); mode == run {
+		r := mux.NewRouter()
+		setupRouter(r)
+		log.Print("RUNNING")
+		err = http.ListenAndServeTLS(":9090", "server.pem", "server.key", r)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	} else if mode == setup {
+		addNewChallenges()
+	}
+
+}
+
+func checkArgs() int {
+
+	if len(os.Args) < 2 {
+		usage()
+	}
+	if os.Args[1] == "setup" {
+		return setup
+	}
+	if os.Args[1] == "run" {
+		return run
+	}
+	return -1
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: ctff [setup|run]")
+	os.Exit(1)
 }
